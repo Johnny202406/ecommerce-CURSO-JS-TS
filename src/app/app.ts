7e763bf76a8ae5b2be3463420196d60bda1472ab
ISokoly <therockplans@gmail.com>
2025-12-15 21:06:42 -0500

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,43 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MOCK_RECIPES } from './mock-recipes';
+import { RecipeModel } from './models';
+import { JsonPipe } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('ecommerce');
+  protected readonly title = signal('My Recipe Box');
+  private valuej = 'hede';
+  name() {
+    console.log('dsubj ');
+  }
+  constructor() {
+    this.nam35e();
+  }
+  nam35e() {
+    console.log('dsubj rg');
+  }
+  incrementar() {
+    this.recipe.set(MOCK_RECIPES[0]);
+    console.log('Botón 1 clickeado');
+  }
+  decrementar() {
+    this.recipe.set(MOCK_RECIPES[1]);
+    console.log('Botón 2 clickeado');
+  }
+  protected recipe: WritableSignal<RecipeModel> = signal(MOCK_RECIPES[0]);
+
+  protected readonly contador = signal(0);
+
+  protected incrementarR(): void {
+    this.contador.update((valorActual) => valorActual + 1);
+  }
+  protected decrementarR(): void {
+    if (this.contador() <= 0) return;
+    this.contador.update((valorActual) => valorActual - 1);
+  }
 }
---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+  imageUrl = input<string>();
+  title = input.required<string>();
+  description = input<string>();
+}
